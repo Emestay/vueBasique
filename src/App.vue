da15b9b63b3c2a3cacf76be2da2898733e0a52c9
Emestay <103575914+Emestay@users.noreply.github.com>
2023-02-21 14:03:55 +0100

    timeoutdemerde

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,47 @@
 <template>
-    
+    <div >
+        <p>{{ message }}</p>
+        <button @click="addToMysteryNumber(5)" >+5</button>
+        <button @click="addToMysteryNumber(1)" >+1</button>
+        <p>{{ mysteryNumber }}</p>
+    </div>
 </template>
   
 <script>
 export default {
     data() {
         return {
-            text1: '',
-            text2: '',
-
+            mysteryNumber: 0,
+            message: "Essaie encore"
         };
     },
     methods: {
+        addToMysteryNumber(a) {
+            this.mysteryNumber += a;
+        },
         
-    }
-};
-</script>
+    },
+    watch: {
+        mysteryNumber(oui) {
+            
+            if (oui === 7) {
+                this.message = oui;
+            } else if (oui > 7) {
+                this.message = "Tu as dépassé le Nbr Mystère";
+            } else {
+                this.message = "Essaie encore";
+            }
+        },
+        
+        
+    },
+    timer(){
+        setTimeout(() => {
+            this.mysteryNumber = 0;
+        }, 5000);
+    },
+    
+}
 
+    ;
+</script>
