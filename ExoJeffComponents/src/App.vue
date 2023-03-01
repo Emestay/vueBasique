0e2414376d995e3d7981d961481429f2a032f1f2
Emestay <103575914+Emestay@users.noreply.github.com>
2023-03-01 09:48:43 +0100

    oui

---
+++ ExoJeffComponents/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Navbar from './components/icons/navbar.vue';
+
+</script>
+
+<template>
+  <header>
+      <Navbar></Navbar>
+  </header>
+  <RouterView />
+</template>
+
---
+++ ExoJeffComponents/src/components/icons/navbar.vue
@@ -0,0 +1,31 @@
+<template>
+    <nav class="bg-gray-800">
+      <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div class="relative flex items-center justify-between h-16">
+          <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+            <div class="flex-shrink-0">
+              <router-link to="/" class="text-white font-bold text-xl">Exo Composant</router-link>
+            </div>
+            <div class="hidden sm:block sm:ml-6">
+              <div class="flex space-x-4">
+                <router-link to="/uno" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Page Uno</router-link>
+                <router-link to="/deux" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">La dos</router-link>
+                <router-link to="/tres" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">La tres</router-link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </template>
+  
+  <script>
+  import { defineComponent } from 'vue'
+  import { RouterLink } from 'vue-router'
+  
+  export default defineComponent({
+    components: {
+      RouterLink
+    }
+  })
+  </script>
